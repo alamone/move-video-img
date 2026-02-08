b48faf0086578f9ecaafe5ff7751ee59a7980d90
alamone <65927878+alamone@users.noreply.github.com>
2026-02-07 17:16:16 -0800

    Add files via upload

--- move-video-img.py
+++ move-video-img.py
@@ -1,4 +1,4 @@
-# move video and image to separate folder
+# move video and image to separate folders
 # requires Python 3.14+ due to using Path.move()
 
 import argparse
@@ -13,17 +13,28 @@ if __name__ == "__main__":
     start_directory = Path(args.folder_name)
     for item_path in start_directory.rglob('*'):
         if item_path.is_file():
-            extension = item_path.suffix
+            extension = item_path.suffix.lower()
             match extension:
                 case ".jpg":
                     item_type = "image"
                 case ".png":
                     item_type = "image"
+                case ".bmp":
+                    item_type = "image"
                 case ".mp4":
                     item_type = "video"
+                case ".mkv":
+                    item_type = "video"
+                case ".avi":
+                    item_type = "video"
+                case ".wmv":
+                    item_type = "video"
+                case ".asf":
+                    item_type = "video"
+                case ".gif":
+                    item_type = "video"                    
                 case _:
                     item_type = "unknown"                 
-            #print(f"[FILE] {item_type} {item_path}")            
             
             path_parts = item_path.parts
             new_path = args.folder_name + "_" + item_type
@@ -34,11 +45,6 @@ if __name__ == "__main__":
             target_file = Path(new_file)
             
             target_path.mkdir(0o777, True, True) #create target path
-            #print (f"item_path: {item_path}")
-            #print (f"new_file: {new_file}")            
                         
             result_path = item_path.move(new_file)
             print (f"{item_path} moved to {result_path}")
-            
-#        elif item_path.is_dir():
-            #print(f"[DIR] {item_path}")
